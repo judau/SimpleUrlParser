8189ae264da47e373056c4b6e1629d03bdb7aa72
log5 <40707031+judau@users.noreply.github.com>
2021-12-20 17:03:51 +0900

    add code

---
+++ main.py
@@ -0,0 +1,31 @@
+import urllib.parse
+import re
+import json
+
+
+def proc_parse(input_value) -> dict:
+    res = {}
+    if "?" not in input_value:
+        return {input_value: "No Appendix"}
+    (url, appendix) = input_value.split("?")
+    apd = {}
+    apd_list = appendix.split("&")
+    for item in apd_list:
+        (a_key, a_val) = item.split("=")
+        if re.search('%\\w\\w', a_val) is not None:
+            a_val = urllib.parse.unquote(a_val)
+        if "?" in a_val:
+            apd[a_key] = proc_parse(a_val)
+        else:
+            apd[a_key] = a_val
+    res[url] = apd
+    return res
+
+
+def main_task():
+    res = proc_parse(input())
+    res_json = json.dumps(res, indent=4)
+    print(res_json)
+
+
+main_task()
